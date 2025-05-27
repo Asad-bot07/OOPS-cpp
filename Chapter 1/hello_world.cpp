350fb931abae401d3635dc5dec05dde3249dba9b
Asad Hussain <asadhussain2408@gmail.com>
2025-05-27 22:03:45 +0530

    Create hello_world.cpp

---
+++ Chapter 1/hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Preprocessor directive to include input-output stream
+using namespace std; // Allows usage of standard namespace (e.g., cout, cin) without std:: prefix
+int main() 
+{ // Main function - execution starts here
+    cout << "Hello, World!"; // Output statement to print text to the console
+    return 0; // Return 0 indicates successful program termination
+}
