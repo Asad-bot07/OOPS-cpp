bf231db1f261a826039b37b0f111f07cf18f7797
Asad Hussain <asadhussain2408@gmail.com>
2025-05-27 22:19:01 +0530

    Create addition.cpp

---
+++ Chapter 2/addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  //Enterthevalueofaandb
+  cout<<"Enter the value of a and b"<<endl;
+  cin>>a>>b;
+  //Printsumofaandb
+  cout<<a+b;
+}
+
+//Output(forinputa=5,b=3):
+//Enter the value of a and b
+//8
