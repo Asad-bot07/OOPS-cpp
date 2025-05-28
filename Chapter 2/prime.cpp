14eab5fb64a13245aa6c56226091f9b56890cfe9
Asad Hussain <asadhussain2408@gmail.com>
2025-05-28 15:49:24 +0530

    Update prime.cpp

--- Chapter 2/prime.cpp
+++ Chapter 2/prime.cpp
@@ -2,30 +2,26 @@
 using namespace std;
 int main()
 {
-    int n,c,i;
-    cout<<"Enter number :"<<endl;
-    cin>>n;
-    if(n==1)
+    int n, c = 0, i; // Initialize c to 0
+    cout << "Enter number :" << endl; // Prompt user for input
+    cin >> n; // Input the numbe
+    if(n == 1)
     {
-        cout<<"Not Prime"<<endl;
+        cout << "Not Prime" << endl; // 1 is not a prime number
     }
     else
     {
-    for(i=2;i<=n/2;i++)
-    {
-        if(n%i==0)
+        for(i = 2; i <= n / 2; i++) // Loop to check divisibility
         {
-            c++;
-            break;
+            if(n % i == 0)
+            {
+                c++; // If divisible, increment counter
+                break; // No need to check further
+            }
         }
-    }
-    if(c)
-    {
-        cout<<"Not Prime"<<endl;
-    }
-    else
-    {
-        cout<<"Prime"<<endl;
-    }
+        if(c)
+            cout << "Not Prime" << endl; // If divisible, not prime
+        else
+            cout << "Prime" << endl; // Else, prime
     }
 }
