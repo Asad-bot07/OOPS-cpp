5f349631b824b9e86c495e2100a5569639f08f54
Asad Hussain <asadhussain2408@gmail.com>
2025-05-28 15:47:44 +0530

    Update leap.cpp

--- Chapter 2/leap.cpp
+++ Chapter 2/leap.cpp
@@ -3,10 +3,19 @@ using namespace std;
 int main()
 {
     int year;
-    cout<<"Enter year :"<<endl;
-    cin>>year;
-    if(year%4==0 && year/100!=0)
-    cout<<"Leap year";
+    cout << "Enter year :" << endl; // Prompt user to enter a year
+    cin >> year; // Input the year
+    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) // Leap year condition
+        cout << "Leap year"; // Output if it's a leap year
     else
-    cout<<"Not a leap year";
+        cout << "Not a leap year"; // Output if it's not a leap year
 }
+/* output1 
+Enter year :
+1900
+Not a leap year
+ output2:
+Enter year :
+2024
+Leap year
+*/
