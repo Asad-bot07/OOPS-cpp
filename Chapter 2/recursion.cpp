8a0e7b4b069f7aa2be3247f611184cddfa9ed6ea
Asad Hussain <asadhussain2408@gmail.com>
2025-05-28 15:51:00 +0530

    Update recursion.cpp

--- Chapter 2/recursion.cpp
+++ Chapter 2/recursion.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
 using namespace std;
+// Recursive function to calculate Fibonacci number at position n
 int fibbo(int n)
 {
-    if(n==0)
-    return 0;
-    else if(n==1)
-    return 1;
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
     else
-    return fibbo(n-1)+fibbo(n-2);
+        return fibbo(n - 1) + fibbo(n - 2);
 }
 int main()
 {
-    int num,i;
-    cout<<"Enter the number of series :\n";
-    cin>>num;
-    for(i=0;i<=num;i++)
+    int num, i;
+    cout << "Enter the number of series :\n"; // Prompt user to enter number of terms
+    cin >> num; // Input the number
+
+    for(i = 0; i <= num; i++) // Print Fibonacci series up to num terms
     {
-        cout<<fibbo(i)<<" ";
+        cout << fibbo(i) << " ";
     }
     return 0;
 }
+/*
+Enter the number of series :
+7
+0 1 1 2 3 5 8 13 
+*/
