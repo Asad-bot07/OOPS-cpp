925e3b7042946077d6815180d013e55414c3476d
Asad Hussain <asadhussain2408@gmail.com>
2025-05-28 15:46:32 +0530

    Update large.cpp

--- Chapter 2/large.cpp
+++ Chapter 2/large.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 using namespace std;
-inline int check(int a,int b,int c)
+// Inline function using ternary operator to find the greatest of three numbers
+inline int check(int a, int b, int c)
 {
-    return (a>b)?(a>c?:c):(b>c?b:c);
+    return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
 }
 int main()
 {
-    int s,q,l,result;
-    cout<<"Enter numbers "<<endl;
-    cin>>s;
-    cin>>q;
-    cin>>l;
-    result=check(s,q,l);
-    cout<<result<<" is greatest!"<<endl;
+    int s, q, l, result;
+    cout << "Enter numbers " << endl; // Prompt user for input
+    cin >> s >> q >> l; // Input three integers
+    result = check(s, q, l); // Call inline function to get greatest number
+    cout << result << " is greatest!" << endl; // Output the result
 }
+/*output
+Enter numbers 
+3 7 5
+7 is greatest!
+*/
