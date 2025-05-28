8cf26269896d8990d3386f248a7ee219c98af76d
Asad Hussain <asadhussain2408@gmail.com>
2025-05-28 15:45:42 +0530

    Update great.cpp

--- Chapter 2/great.cpp
+++ Chapter 2/great.cpp
@@ -1,30 +1,34 @@
 #include<iostream>
 using namespace std;
-int grea(int v,int m,int n)
+// Function to find the greatest among three numbers
+int grea(int v, int m, int n)
 {
-    if(v>m && v>n)
-    return v;
-    else if(m>v && m>n)
-    return m;
-    else if(n>v && n>m)
-    return n;
+    if(v > m && v > n)
+        return v;
+    else if(m > v && m > n)
+        return m;
+    else if(n > v && n > m)
+        return n;
     else
-    return 0;
+        return 0; // Return 0 if all numbers are equal
 }
-inline int check(int a,int b,int c)
+// Inline function that calls the grea() function
+inline int check(int a, int b, int c)
 {
-    return grea(a,b,c);
+    return grea(a, b, c);
 }
 int main()
 {
-    int s,q,l,result;
-    cout<<"Enter numbers "<<endl;
-    cin>>s;
-    cin>>q;
-    cin>>l;
-    result=check(s,q,l);
-    if(result==0)
-    cout<<"All are equal!!";
+    int s, q, l, result;
+    cout << "Enter numbers " << endl; // Prompt user for input
+    cin >> s >> q >> l; // Input three integers
+    result = check(s, q, l); // Call inline function to find greatest
+    if(result == 0)
+        cout << "All are equal!!"; // All values are equal
     else
-    cout<<result<<" is greatest!"<<endl;
+        cout << result << " is greatest!" << endl; // Output the greatest value
 }
+/*output
+Enter numbers 
+5 9 2
+9 is greatest!/*/
